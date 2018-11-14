79b200dc246d7673ce8c6a13df9f01d428dda254
Jakku Man <32721112+Jakkuman@users.noreply.github.com>
2018-11-14 18:40:47 -0500

    Added Pseudo code slots
    Added the space for Pseudo code for the Climber and the Gripper.

--- src/org/usfirst/frc/team1559/robot/Gripper.java
+++ src/org/usfirst/frc/team1559/robot/Gripper.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team1559.robot;
 
 public class Gripper {
-
-}
+	/*
+	 * "Insert Pseudo code here"
+	 * Pseudo code is basically writing comments to describe what your code is going to do. In other words, in this space
+	 * provided, write in basic English what you want your code to do.
+	 */}

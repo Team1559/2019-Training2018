e8c891f426aa8936c1bf2ca3cfe2a186dca3c16f
Eric-Marchetti <44625126+Eric-Marchetti@users.noreply.github.com>
2018-11-28 18:56:22 -0500

    Update src/org/usfirst/frc/team1559/robot/Gripper.java
    Pseudo code for gripper

--- src/org/usfirst/frc/team1559/robot/Gripper.java
+++ src/org/usfirst/frc/team1559/robot/Gripper.java
@@ -5,4 +5,15 @@ public class Gripper {
 	 * "Insert Pseudo code here"
 	 * Pseudo code is basically writing comments to describe what your code is going to do. In other words, in this space
 	 * provided, write in basic English what you want your code to do.
-	 */}
+	 *
+	 *ROBOT WILL GRAB BLOCK WHEN RIGHT TRIGGER IS PRESSED
+	 *ROBOT WILL GO TO ALMOST CLOSED IF THE LEFT TRIGGER0
+	 *IS PRESSED
+	 *ALL OF THESE FUNCTIONS WILL BE TOGGLEABLE
+	 *
+	 */
+	
+
+
+
+}

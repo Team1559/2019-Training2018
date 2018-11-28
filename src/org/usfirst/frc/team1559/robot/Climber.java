5368af6edb1b4845dad000ce4def9f60b063df63
Jason-VL <jasonevanderlinden@gmail.com>
2018-11-28 18:57:07 -0500

    Update src/org/usfirst/frc/team1559/robot/Climber.java

--- src/org/usfirst/frc/team1559/robot/Climber.java
+++ src/org/usfirst/frc/team1559/robot/Climber.java
@@ -1,5 +1,17 @@
 package org.usfirst.frc.team1559.robot;
 
-public class Climber {
+import org.usfirst.frc.team1559.robot.Robot;
+import org.usfirst.frc.team1559.robot.Wiring;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Spark;
+
+public class Climber {
+// Extend arm upward and stop very close to max height
+// Pull arm down and keep pulling so robot doesn't fall.
+//Activate motor attached to rope(Can't remember name, remeber to get it) and keep spinning until user lets go.
 }
